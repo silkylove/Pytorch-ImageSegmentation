f674a6ccfb4eb83a926f6f589045aadf166c0051
huang <353837214@qq.com>
2018-12-23 18:43:22 +0800

    update

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ All backbones
 ['resnet18', 'resnet34', 'resnet50', 'resnet101',
  'resnet152', 'senet154', 'se_resnet50', 'se_resnet101',
  'se_resnet152', 'se_resnext50_32x4d', 'se_resnext101_32x4d',
- 'mobilenet_v2', 'shufflenet_v2' (for deeplabv3+)]
+ 'mobilenet_v2', 'shufflenet_v2'(for deeplabv3+)]
 '''
 
 unet_params = {'filter_scale': 1}
